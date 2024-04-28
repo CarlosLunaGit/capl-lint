f236679861a29657f1abfeb19cefdf25c6f32c86
Carlos Lizarde <carlos.lizarde@volvocars.com>
2024-04-28 17:00:43 +0200

    updating test case

--- tests/functions/lintCode.test.js
+++ tests/functions/lintCode.test.js
@@ -1,7 +1,7 @@
 const { lintCode } = require('../../src/core/lintCode');
 
 test('lintCode detects missing semicolons', () => {
-    const inputCode = 'int x = 10\nint y = 20';
+    const inputCode = 'int x = 10\\nint y = 20';
     const expectedErrors = [
         { line: 1, error: 'Line does not end with a semicolon.' },
         { line: 2, error: 'Line does not end with a semicolon.' }
